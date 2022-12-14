f4e7c5c9e9f94ee7b550cfe36f809dc390f3127b
mylezuiru <mylesuiru@gmail.com>
2022-12-14 12:23:40 +0300

    completed assignment

---
+++ README.md
@@ -0,0 +1,77 @@
+Flatdango Movie Theater
+
+
+Description 
+ 
+ This is an application that allows a user to buy ticket for movies and be able to check for movies that are coming up.
+
+ Development Installation
+
+ Requirments
+
+ ​In order for you to use the content on this repo ensure you have the following:​
+
+    A computer that runs on either of the following; (Windows 7+, Linux, Mac OS).
+
+    *nodejs 9.0+ 
+
+    *Visual Studio Code.
+
+    *Live server extension.
+
+    Alternative One
+
+   .Open a terminal / command line interface on your computer.
+
+
+
+   .Clone the repo by using the following to create a copy on your local machine:
+
+      git clone https://github.com/mylezuiru/Flatdango
+
+    .Change directory to the repo folder:
+
+       cd  Flatdango
+
+    .Open it in Visual Studio Code
+
+       code .
+
+    Alternative Two
+
+     On the top right corner of this page there is a button labelled Fork.
+
+      Click on that button to fork the repo to your own account.
+
+      Take on the process in Alternative One above.
+
+      Remember to replace your username when cloning.
+          git clone https://github.com/your-username-here/Flatdango
+
+
+Running the application
+
+​    To run the application, you can use the following steps to run the app.​
+
+
+    Install json-server​npm install json-server​.
+
+
+    Then follow instructions here for live server to work well with json-server​.
+
+
+    Run the following command to get backend started:​json-server --watch db.json.
+
+
+    Right click on the index.html and navigate to Open With Live Server option, to open the application on your browser.
+
+
+     Repeat the steps on the Features section below.
+
+     Features
+
+     On the page you are able to see a movie list.
+
+      Click on any movie's Title on the left side of the web page to display the movie's details.
+
+      Click on the buy ticket in the movie details to buy .
---
+++ index.js
@@ -0,0 +1,108 @@
+// Function used to fetch data from JSON server (db.json file)
+function fetchMoviesList(){
+    fetch("http://localhost:3000/films")
+    .then(response => response.json())
+    .then(data => generateMovieList(data))
+}
+
+/*
+This function takes the data fetched and uses it to render the list of movies
+*/
+function generateMovieList(movies){
+
+    // grabs unordered list film
+    const films = document.getElementById('films')
+    const deleteMovie = document.getElementById('delete_movie')
+
+    movies.forEach(movie => {
+
+        // create a link that also serves as list element
+        const list = document.createElement('a')
+        list.href ="#"
+        list.innerText = movie.title
+        list.classList.add('film-item','list-group-item' )
+
+        //append list element to the ul element
+        films.appendChild(list)
+
+        // eventLister for the list element if clicked to display movie details
+        list.addEventListener('click', (e) => {
+            e.preventDefault()
+            generateMovieDetails(movie)
+        })
+    }); 
+}
+
+// function used to generate movie details to the right side of the webpage
+function generateMovieDetails(movie){
+
+    // selects divs for displaying image and movie details
+    const movieImage = document.getElementById('movie_image')
+    const movieDescription= document.getElementById('movie_description')
+
+    const remainingSeats = movie.capacity - movie.tickets_sold
+
+    // image and book ticket form is rendered in the image div if the movie is selected
+    movieImage.innerHTML =
+    `<img src="${movie.poster}" alt="movie poster" height="200px" width="150px"></img>
+    <form id="form_book">
+        <label class="form-label" >Number of tickets: </label>
+        <input id="tickets" type="number" placeholder="Input Number of tickets" class="form-control" style="width:400px" min="0" step="1" max="${remainingSeats}" required>
+        <button type="submit" class="btn btn-primary mt-2" id="book_ticket">Book Ticket</button>
+        
+    </form>
+    `
+    
+    // html element rendered in the decription div if the movie is selected
+    movieDescription.innerHTML = 
+        `
+         <h4>${movie.title}</h4>
+         <p>${movie.description}</p>
+         <p>Show Time: ${movie.showtime}</p>
+         <p id="seats">Remaining Seats: ${remainingSeats}</p>
+        `
+    const tickets = document.getElementById('tickets').value
+    
+    // if condition that makes changes to button and input field when the seats are sold out
+    if(remainingSeats === 0){
+            document.getElementById('tickets').disabled = true
+            const button = document.getElementById('book_ticket')
+            button.innerText = 'Sold Out'
+            button.disabled = true
+        }
+
+    // get the form for booking movie tickets
+    const form = document.getElementById('form_book')
+
+        // eventListener for form submit used to book
+        form.addEventListener('submit', (e) => {
+            patchMovieDetails(movie) 
+            form.reset()
+        }) 
+    
+}
+
+// function for patching data to server
+function patchMovieDetails(movie){
+
+    const remainingSeats = movie.capacity - movie.tickets_sold
+    const tickets = document.getElementById('tickets').value
+    movie.tickets_sold += parseInt(tickets,10)
+
+    // function used to patch new information to the server
+    fetch(`http://localhost:3000/films/${movie.id}`,{
+        method: 'PATCH',
+        headers: {'Content-Type' : 'application/json'},
+        body: JSON.stringify(movie)
+    })
+    .then(response => response.json())
+    .then(data => data)
+
+    // alert the user on the number of tickets bought
+    alert(`Thank you!\n You have bought ${tickets} tickets\n for ${movie.title}`)
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchMoviesList()
+})
